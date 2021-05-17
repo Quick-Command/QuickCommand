51707ac8127202b08e3d115587cd6bf72c25febe
JS <jeff.shep777@gmail.com>
2021-05-17 17:49:15 -0600

    Add ChartView component skeleton

---
+++ src/components/ChartView/ChartView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ChartView.css'
+
+const ChartView = () => {
+
+  return (
+    <section className='chart-view-container'>
+      test
+    </section>
+  )
+
+}
+
+export default ChartView
