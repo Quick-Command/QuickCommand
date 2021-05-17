9d9c08a41c5b18b012f2b2f23731bd8a17707fe5
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-17 13:06:28 -0600

    Build out add contact form with necessary post request

--- src/API-calls.js
+++ src/API-calls.js
@@ -24,3 +24,14 @@ export const postNewIncident = (incidentObj) => {
   })
   .then(checkResponse)
 }
+
+export const addNewContact = (newContact) => {
+  return fetch(`${baseURL}/contacts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newContact)
+  })
+  .then(checkResponse)
+}
