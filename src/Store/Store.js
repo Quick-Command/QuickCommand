fb520e997383bccd566bc1363f476bfd9881c0a9
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-17 15:36:04 -0600

    Update Store

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import DeclareIncidentFormReducer from '../components/DeclareIncidentForm/DeclareIncidentFormSlice'
-import IncidentListReducer from '../components/IncidentList/IncidentListSlice'
+import IncidentListReducer from '../Components/IncidentList/IncidentListSlice'
 
 export const store = configureStore({
   reducer: {
-    declareIncidentForm: DeclareIncidentFormReducer,
-    ongoingIncidents: IncidentListReducer,
-
+    incidentList: IncidentListReducer
   },
 });
