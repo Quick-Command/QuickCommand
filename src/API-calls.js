6902ef4216f8fc221c64f58e6e6a96809ad1329b
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-17 15:35:02 -0600

    Add fetch requests to API calls file

--- src/API-calls.js
+++ src/API-calls.js
@@ -1,7 +1,36 @@
+const baseURL = `https://qc-engine.herokuapp.com/api/v1`
 
-// const baseURL = ``
+const checkResponse = (response) => {
+  if (!response.ok) {
+    throw new Error()
+  } else {
+    return response.json()
+  }
+}
 
-// export const getActiveIncidents = () => {
-//   return fetch(`${baseURL}/api/v1/incidents?open=true`)
-//   .then(response => response.json())
-// }
+export const fetchOngoingIncidents = () => {
+  return fetch(`${baseURL}/incidents?open=true`)
+    .then(checkResponse)
+}
+
+export const postNewIncident = (incidentObj) => {
+  return fetch(`${baseURL}/incidents`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(incidentObj)
+  })
+  .then(checkResponse)
+}
+
+export const addNewContact = (newContact) => {
+  return fetch(`${baseURL}/contacts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newContact)
+  })
+  .then(checkResponse)
+}
