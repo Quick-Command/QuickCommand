bb8b26ef09d8e77972ad04bd6daad4d2f9b632d4
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-18 15:05:13 -0600

    Comment out searchResultsReducer on store

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import OngoingIncidentsReducer from '../Components/OngoingIncidents/OngoingIncidentsSlice'
-import ResolvedIncidentsReducer from '../Components/ResolvedIncidents/ResolvedIncidentsSlice'
-import searchResultsReducer from '../Components/DatabaseSearch/DatabaseSearchSlice'
-// TRAVIS TEST 2
+import OngoingIncidentsReducer from '../Components/OngoingIncidents/OngoingIncidentsSlice';
+import ResolvedIncidentsReducer from '../Components/ResolvedIncidents/ResolvedIncidentsSlice';
+// import SearchResultsReducer from '../Components/DatabaseSearch/DatabaseSearchSlice';
+
 export const store = configureStore({
   reducer: {
     ongoingIncidents: OngoingIncidentsReducer,
     resolvedIncidents: ResolvedIncidentsReducer,
-    searchResults: searchResultsReducer
+    // searchResults: SearchResultsReducer
   },
 });
