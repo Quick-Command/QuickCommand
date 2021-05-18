7f04a6a38bc0559a325fe175681795b37b855bf2
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-18 15:38:06 -0600

    Make components lowercase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/app/App';
+import App from './components/app/App';
 import { store } from './Store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
