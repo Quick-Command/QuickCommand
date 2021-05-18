9bc9e8b5897e54578ff1b5d23f1edfe84e26382d
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-05-18 14:15:37 -0600

    Travis test II

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import OngoingIncidentsReducer from '../Components/OngoingIncidents/OngoingIncidentsSlice'
 import ResolvedIncidentsReducer from '../Components/ResolvedIncidents/ResolvedIncidentsSlice'
 import searchResultsReducer from '../Components/DatabaseSearch/DatabaseSearchSlice'
-
+// TRAVIS TEST 2
 export const store = configureStore({
   reducer: {
     ongoingIncidents: OngoingIncidentsReducer,
