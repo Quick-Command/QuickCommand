823ef6ad3796734cd31c79e8117b64b3cb9b40ab
percworld <percworld@gmail.com>
2021-05-22 13:50:46 -0600

    Chage stylings for links and move icon function to utilities

--- src/components/OngoingIncidents/OngoingIncidents.js
+++ src/components/OngoingIncidents/OngoingIncidents.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { formatDate } from '../../utilities'
 import {
   getOngoingIncidents
 } from './OngoingIncidentsSlice'
 import './OngoingIncidents.css'
-import { ReactComponent as Fire } from '../../Icons/gripfire-brands.svg'
+import { getIcon, formatDate } from '../../utilities'
 
 const OngoingIncidents = () => {
 
@@ -16,12 +15,14 @@ const OngoingIncidents = () => {
     dispatch(getOngoingIncidents())//req adds results to state
   }, [dispatch])
 
+
+
   const ongoingIncidents = useSelector(state => state.ongoingIncidents.ongoingIncidents).map(incident => {
     return (
       <Link className='incident-container' to={`/incident/${incident.id}`} key={incident.id}>
         <div className='incident' >
           <h3>
-            <span><Fire className='element'></Fire></span>{incident.attributes.type}{incident.attributes.name}
+            <span>{getIcon(incident)}</span>{incident.attributes.type}{incident.attributes.name}
           </h3>
           <p>* DECLARED: {formatDate(incident.attributes.start_date)}</p>
         </div>
@@ -34,8 +35,8 @@ const OngoingIncidents = () => {
       <h2 data-cy='ongoing-incidents' className='incident-text'>ONGOING INCIDENTS:</h2>
       <section className='ongoing-container' >
         {ongoingIncidents}
-      </section >
-    </div >
+      </section>
+    </div>
   )
 
 }
