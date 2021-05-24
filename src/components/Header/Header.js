7cbaa03041b57ded4c1bdacc4860e04b78ffa77b
JS <jeff.shep777@gmail.com>
2021-05-24 17:03:15 -0600

    Update header component positioning

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,10 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <img className='logo' src={'/qc-logo-8.png'} alt='Quick Command Logo'></img>
-      <h1 className='app-name'>QuickCommand</h1>
+      <section className="logo-container">
+        <img className='logo' src={'/qc-logo-8.png'} alt='Quick Command Logo'></img>
+        <h1 className='app-name'>uickCommand</h1>
+      </section>
       <nav className='header-links'>
         <NavLink to='/database' className='nav-link' data-cy='database-btn'>DATABASE</NavLink>
         <div className='btn-dash'></div>
